9683bda9f3f3dbda103d1ffd87a8238747706fe9
Pavel <jazz.fog@gmail.com>
2016-01-28 11:39:05 -0800

    no message

--- upload.php
+++ upload.php
@@ -3,8 +3,8 @@
 // Dir of your choice
 $dir = __DIR__.'/files/';
 
-//Keep in mind that file extension may be different, check it using $_POST['uplFileName']
-$safeName = date('Y-m-d_H-i-s').'.jpg';
+//Keep in mind that file extension may be different, check uploaded file
+$safeName = uniqid(date('Y-m-d_H-i-s')).'.jpg';
 
 // Uncomment to see all the passed variables
 //print_r($_POST);
